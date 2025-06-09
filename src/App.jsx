ce4215cba63f00b07477597d173b435c6ff967c9
nairx <nairxprv@gmail.com>
2025-06-09 12:58:01 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Header from "./Header";
+import Footer from "./Footer";
+import Home from "./Home";
+function App() {
+  return (
+    <div>
+      <Header />
+      <Home />
+      <Footer />
+    </div>
+  );
+}
+export default App;
