74a5a7dd755e6e729ecfa003f5a4da1562686bb9
nairx <nairxprv@gmail.com>
2025-06-10 14:38:01 +0530

    Updated

--- src/Header.jsx
+++ src/Header.jsx
@@ -13,6 +13,9 @@ export default function Header({ name }) {
             <Link to="/">Home</Link>
           </li>
           <li>
+            <Link to="/cart">Cart</Link>
+          </li>
+          <li>
             <Link to="/login">Login</Link>
           </li>
         </ul>
